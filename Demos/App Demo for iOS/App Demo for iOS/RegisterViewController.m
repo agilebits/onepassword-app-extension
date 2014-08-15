a516cb1770e06c871f454718e5368a938dd5b008
Rad Azzouz <rad@agilebits.com>
2014-08-14 20:43:35 -0700

    Added useful comment about password generation options

--- Demos/App Demo for iOS/App Demo for iOS/RegisterViewController.m
+++ Demos/App Demo for iOS/App Demo for iOS/RegisterViewController.m
@@ -46,6 +46,7 @@
 		}
 	};
 	
+	// Password generation options are optional, but are very handy in case you have strict rules about password lengths
 	NSDictionary *passwordGenerationOptions = @{
 		AppExtensionGeneratedPasswordMinLengthKey: @(6),
 		AppExtensionGeneratedPasswordMaxLengthKey: @(50)
