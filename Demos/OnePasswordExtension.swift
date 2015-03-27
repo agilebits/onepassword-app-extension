1651e8dda09675c8e7f915353d6c2b7302026fa8
Diego <ghecho@gmail.com>
2015-03-27 00:44:53 -0600

    Add note to users
    Signed-off-by: Diego <ghecho@gmail.com>

--- Demos/OnePasswordExtension.swift
+++ Demos/OnePasswordExtension.swift
@@ -39,6 +39,11 @@ let AppExtensionErrorCodeFillFieldsScriptFailed = 5
 let AppExtensionErrorCodeUnexpectedData = 6
 let AppExtensionErrorCodeFailedToObtainURLStringFromWebView = 7
 
+// Note to creators of libraries or frameworks:
+// If you include this code within your library, then to prevent potential duplicate symbol
+// conflicts for adopters of your library, you should rename the OnePasswordExtension class.
+// You might to so by adding your own project prefix, e.g., MyLibraryOnePasswordExtension.
+
 class OnePasswordExtension: NSObject {
    
 }
