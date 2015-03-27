1eb96a6a69fefc65f55d95f7f24e0fce0971c818
Diego <ghecho@gmail.com>
2015-03-27 00:10:17 -0600

    Add OnePasswordExtension.swift
    Starting with the Swift translation
    
    Signed-off-by: Diego <ghecho@gmail.com>

---
+++ Demos/OnePasswordExtension.swift
@@ -0,0 +1,16 @@
+//
+//  OnePasswordExtension.swift
+//  App Demo for iOS
+//
+//  Created by Diego Márquez Arzate on 3/27/15.
+//  Copyright (c) 2015 Diego. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+import WebKit
+
+class OnePasswordExtension: NSObject {
+   
+}
