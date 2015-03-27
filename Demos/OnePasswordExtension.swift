d4ce114835243acef29a02bf694514c5c44cb2cd
Diego <ghecho@gmail.com>
2015-03-27 00:23:11 -0600

    Add global constants
    Signed-off-by: Diego <ghecho@gmail.com>

--- Demos/OnePasswordExtension.swift
+++ Demos/OnePasswordExtension.swift
@@ -11,6 +11,34 @@ import UIKit
 import MobileCoreServices
 import WebKit
 
+// Login Dictionary keys
+let AppExtensionURLStringKey = "url_string"
+let AppExtensionUsernameKey = "username"
+let AppExtensionPasswordKey = "password"
+let AppExtensionTitleKey = "login_title"
+let AppExtensionNotesKey = "notes"
+let AppExtensionSectionTitleKey = "section_title"
+let AppExtensionFieldsKey = "fields"
+let AppExtensionReturnedFieldsKey = "returned_fields"
+let AppExtensionOldPasswordKey = "old_password"
+let AppExtensionPasswordGereratorOptionsKey = "password_generator_options"
+
+// Password Generator options
+let AppExtensionGeneratedPasswordMinLengthKey = "password_min_length"
+let AppExtensionGeneratedPasswordMaxLengthKey = "password_max_length"
+
+// Errors
+let AppExtensionErrorDomain = "OnePasswordExtension"
+
+let AppExtensionErrorCodeCancelledByUser = 0
+let AppExtensionErrorCodeAPINotAvailable = 1
+let AppExtensionErrorCodeFailedToContactExtension = 2
+let AppExtensionErrorCodeFailedToLoadItemProviderData = 3
+let AppExtensionErrorCodeCollectFieldsScriptFailed = 4
+let AppExtensionErrorCodeFillFieldsScriptFailed = 5
+let AppExtensionErrorCodeUnexpectedData = 6
+let AppExtensionErrorCodeFailedToObtainURLStringFromWebView = 7
+
 class OnePasswordExtension: NSObject {
    
 }
