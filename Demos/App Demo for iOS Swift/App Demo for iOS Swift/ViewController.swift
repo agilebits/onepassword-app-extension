d0df0d54cf406d5b270b7cc575efafcea91c9a1e
Diego <ghecho@gmail.com>
2015-03-31 01:30:10 -0600

    Create Swift Demo Project
    Signed-off-by: Diego <ghecho@gmail.com>

---
+++ Demos/App Demo for iOS Swift/App Demo for iOS Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Demo for iOS Swift
+//
+//  Created by Diego on 3/31/15.
+//  Copyright (c) 2015 Agilebits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
