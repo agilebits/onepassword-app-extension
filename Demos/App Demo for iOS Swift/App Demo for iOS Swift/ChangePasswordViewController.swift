38c893d1d037997718fcf1f054ff6969bbad8526
Rad Azzouz <rad@agilebits.com>
2015-07-28 20:01:56 -0400

    Renamed variable: `newPassword` -> `changedPassword`

--- Demos/App Demo for iOS Swift/App Demo for iOS Swift/ChangePasswordViewController.swift
+++ Demos/App Demo for iOS Swift/App Demo for iOS Swift/ChangePasswordViewController.swift
@@ -26,18 +26,18 @@ class ChangePasswordViewController: UIViewController {
 	}
 	
 	@IBAction func changePasswordIn1Password(sender:AnyObject) -> Void {
-		var newPassword = self.freshPasswordTextField.text!
+		var changedPassword = self.freshPasswordTextField.text!
 		var oldPassword = self.oldPasswordTextField.text!
 		var confirmationPassword = self.confirmPasswordTextField.text!
 
 		// Validate that the new password and the old password are not the same.
-		if (count(oldPassword) > 0 && oldPassword == newPassword) {
+		if (count(oldPassword) > 0 && oldPassword == changedPassword) {
 			self.showChangePasswordFailedAlertWithMessage("The old and the new password must not be the same")
 			return;
 		}
 
 		// Validate that the new and confirmation passwords match.
-		if (count(newPassword) > 0 && newPassword != confirmationPassword) {
+		if (count(changedPassword) > 0 && changedPassword != confirmationPassword) {
 			self.showChangePasswordFailedAlertWithMessage("The new passwords and the confirmation password must match")
 			return;
 		}
@@ -50,7 +50,7 @@ class ChangePasswordViewController: UIViewController {
 		let newLoginDetails:[String: AnyObject] = [
 			AppExtensionTitleKey: "ACME", // Optional, used for the third schenario only
 			AppExtensionUsernameKey: "aUsername", // Optional, used for the third schenario only
-			AppExtensionPasswordKey: newPassword,
+			AppExtensionPasswordKey: changedPassword,
 			AppExtensionOldPasswordKey: oldPassword,
 			AppExtensionNotesKey: "Saved with the ACME app", // Optional, used for the third schenario only
 		]
