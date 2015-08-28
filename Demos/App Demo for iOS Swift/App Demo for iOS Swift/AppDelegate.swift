c5dd6e7ae6220c85d2d95a1892a475f20c8b62d9
Rad Azzouz <rad@agilebits.com>
2015-08-27 21:18:33 -0400

    Removed unused UIWindow properties from AppDelegate from the Swift apps

--- Demos/App Demo for iOS Swift/App Demo for iOS Swift/AppDelegate.swift
+++ Demos/App Demo for iOS Swift/App Demo for iOS Swift/AppDelegate.swift
@@ -10,9 +10,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-	var window: UIWindow?
-
+	
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
 		return true
