da69d6f9423ad052032a6ec03fa124b0c7c3da30
Rad Azzouz <rad@agilebits.com>
2015-10-17 14:10:26 -0400

    [Release] 1.6.3

--- OnePasswordExtension.m
+++ OnePasswordExtension.m
@@ -8,7 +8,7 @@
 #import "OnePasswordExtension.h"
 
 // Version
-#define VERSION_NUMBER @(162)
+#define VERSION_NUMBER @(163)
 static NSString *const AppExtensionVersionNumberKey = @"version_number";
 
 // Available App Extension Actions
