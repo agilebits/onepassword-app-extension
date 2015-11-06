957741f52d8747aab9b992d5fce7ad6a88583d90
Rad Azzouz <rad@agilebits.com>
2015-11-06 17:00:02 -0500

    Version number bump.

--- OnePasswordExtension.m
+++ OnePasswordExtension.m
@@ -8,7 +8,7 @@
 #import "OnePasswordExtension.h"
 
 // Version
-#define VERSION_NUMBER @(163)
+#define VERSION_NUMBER @(164)
 static NSString *const AppExtensionVersionNumberKey = @"version_number";
 
 // Available App Extension Actions
